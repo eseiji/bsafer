527b0bb6a489233990857e4789f6c0b1c2ff231c
Enzo Seiji <enzoseiji14@gmail.com>
2022-06-12 20:32:16 -0300

    backup

--- lib/view/about/components/about_card.dart
+++ lib/view/about/components/about_card.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AboutCard extends StatelessWidget {
   const AboutCard({
